113c769250090c39d527a8e33c95036b674381be
albourgade1 <alexis.bourgade@etu.uca.fr>
2021-01-06 13:39:13 +0100

    t

--- index.php
+++ index.php
@@ -3,10 +3,16 @@
     //chargement autoloader pour autochargement des classes
     require_once(__DIR__ . '/Config/Autoloader.php');
     Autoloader::load();
+
+    /**
+     * on parse le fichier à chaque appelle de l'index, ce n'est pas la meilleur des solutions et il faudrait creer
+     * un autre programme qui le fait de manière periodique
+     */
     if (@fsockopen("www.google.fr", 80)) { //reelement utile uniquement si on heberge localement le site
         new ParseurXml();
         new FrontController();
-    } else {
+    }
+    else {
         $tVueErreur[] = "Pas de connection à internet";
         require($rep . $vues['error']);
     }
